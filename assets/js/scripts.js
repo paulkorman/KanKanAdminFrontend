39af74d5ffe993e2c4e5b4919d9f3c652e92ba0f
pasha <fastist@gmail.com>
2017-04-25 13:39:10 -0600

    add sponsor

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,27 @@
- $(document).ready(function() {
+// Display the number of characters in textarea
+
+(function ($) {
+
+    $.fn.limit = function (elem, limit) {
+
+        var target = this,
+            display = $(elem);
+
+        target.on('focus keyup setlimit', function () {
+            var len = target.val().length;
+            //console.log(len);
+            var maxlen = target.attr("maxlength");
+            console.log((limit-len) +',' + len);
+            //display.text(limit - len);
+            $(elem).next('.limit-js').text(len + ' / ' + limit);
+            $(elem).attr('maxlength', limit)
+        }).trigger('setlimit');
+
+    };
+
+}(jQuery));
+
+$(document).ready(function() {
         // Initializes search overlay plugin.
         // Replace onSearchSubmit() and onKeyEnter() with 
         // your logic to perform a search and display results
@@ -27,4 +50,6 @@
                 $(this).data('timer', wait);
             }
         });
-    })
+        $("textarea").limit("#ads_text_input-js", 50); // и число не строкой
+    });
+
